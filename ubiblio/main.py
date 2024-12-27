e28cd11cccf5e8453765d5209a1f49a1db803070
Sean Boyce <sean.a.boyce@gmail.com>
2024-12-27 08:44:28 +0700

    Update main.py
    Hotfix: if isbn autoadd fails, try another provider

--- ubiblio/main.py
+++ ubiblio/main.py
@@ -453,7 +453,12 @@ def searchbookTitle(request: Request, user: schemas.User = Depends(get_current_u
 def new_isbn(isbn, request: Request, user: schemas.User = Depends(get_current_user_from_token)):
     try:
         if user.isAdmin == True:
-            book = meta(isbn)
+            book = meta(isbn,service='goob')
+            if not "Title" in book:
+                book = meta(isbn,service='openl')
+            if not "Title" in book:
+                book = meta(isbn,service='wiki')
+                print(book["Title"])
             title = book["Title"]
             author = book["Authors"][0]
             try:
