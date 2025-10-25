7dc450f88cfeb0b6ed5391bf1e338db5129e5f03
Axel Goris <axelgoris99@gmail.com>
2025-10-25 16:08:11 +0200

    Feat: Add a way to reload the app on file changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Development mode: if DEV=true, run uvicorn with reload and do not change ownership
+if [ "$DEV" = "true" ] || [ "$DEV" = "1" ]; then
+  exec uvicorn ubiblio.main:app --host 0.0.0.0 --port 8000 --reload --forwarded-allow-ips '*' --proxy-headers
+fi
+
+# Production/default behaviour: create user if PUID/PGID are provided and run as that user
 if [ -z "$PUID" ] || [ -z "$PGID" ]; then
   exec uvicorn ubiblio.main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips '*' --proxy-headers
 else
