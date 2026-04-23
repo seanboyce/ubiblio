f1379cb9fc12180704b46d56f21ce0185cd932b4
Garima Singh <igarimasingh@gmail.com>
2026-04-23 21:38:55 +1000

    (Issue #59 and Issue #54) Allow scanning a book without Author
    Two Major Changes:
    * Allow adding using Scan or by ISBN for books without Author #59
    * ISBN auto-add feature makes two calls to openLibrary, only one is
      necessary #54

--- tests/routers/books/test_book_metadata_client.py
+++ tests/routers/books/test_book_metadata_client.py
@@ -2,6 +2,7 @@ from urllib.parse import urlencode
 
 from pytest import MonkeyPatch
 import responses
+import pytest
 
 from ubiblio.routers.books.book_metadata_client import BookMetadataClient
 
@@ -100,26 +101,22 @@ class TestBookMetadataClientOpenLibraryByIsbn:
 
         responses.add(
             responses.GET,
-            f"{BookMetadataClient.OPEN_LIBRARY_API}isbn/{isbn}.json",
+            f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys=9780123456789&format=json&jscmd=details",
             json={
-                "title": "Test Title",
-                "authors": [
-                    {
-                        "key": "/authors/OL1607920A"
+                f"{isbn}": {
+                    "details": {
+                        "title": "Test Title",
+                        "authors": [
+                            {
+                                "key": "/authors/OL1607920A",
+                                "name": "A. Reader"
+                            }
+                        ],
+                        "description": {
+                            "value": "A fine book.",
+                        }
                     }
-                ],
-                "description": {
-                    "value": "A fine book.",
                 }
-            },
-            status=200,
-        )
-
-        responses.add(
-            responses.GET,
-            f"{BookMetadataClient.OPEN_LIBRARY_API}/authors/OL1607920A.json",
-            json={
-                "personal_name": "A. Reader",
             },
             status=200,
         )
@@ -135,17 +132,87 @@ class TestBookMetadataClientOpenLibraryByIsbn:
             "Author": "A. Reader",
             "Summary": "A fine book.",
         }
-        assert len(responses.calls) == 2
-        assert responses.calls[0].request.url == f"{BookMetadataClient.OPEN_LIBRARY_API}isbn/{isbn}.json"
-        assert responses.calls[1].request.url == f"{BookMetadataClient.OPEN_LIBRARY_API}/authors/OL1607920A.json"
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details"
 
     @responses.activate
-    def test_open_library_by_isbn_should_return_error_when_response_is_not_ok(self, monkeypatch: MonkeyPatch) -> None:
+    @pytest.mark.parametrize("arthur_value", [[], [{}], [{"key": "/authors/OL1607920A"}]])
+    def test_open_library_by_isbn_should_return_book_metadata_even_if_no_author_is_found(self, arthur_value: list[dict[str, str]]) -> None:
+        # Arrange
+        isbn = "9780123456789"
+
+        responses.add(
+            responses.GET,
+            f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details",
+            json={
+                f"{isbn}": {
+                    "details": {
+                        "title": "Test Title",
+                        "authors": arthur_value,
+                        "description": {
+                            "value": "A fine book.",
+                        }
+                    }
+                }
+            },
+            status=200,
+        )
+
+        client = BookMetadataClient()
+
+        # Act
+        book, status = client.open_library_by_isbn(isbn)
+
+        # Assert
+        assert status == 200
+        assert book == {
+            "Title": "Test Title",
+            "Summary": "A fine book.",
+            "Author": "",
+        }
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details"
+
+    @responses.activate
+    def test_open_library_by_isbn_should_return_book_metadata_even_if_no_description_is_found(self, monkeypatch: MonkeyPatch) -> None:
+        # Arrange
+        isbn = "9780123456789"
+
+        responses.add(
+            responses.GET,
+            f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details",
+            json={
+                f"{isbn}": {
+                    "details": {
+                        "title": "Test Title",
+                    }
+                }
+            },
+            status=200,
+        )
+
+        client = BookMetadataClient()
+
+        # Act
+        book, status = client.open_library_by_isbn(isbn)
+
+        # Assert
+        assert status == 200
+        assert book == {
+            "Title": "Test Title",
+            "Summary": "",
+            "Author": "",
+        }
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details"
+
+    @responses.activate
+    def test_open_library_by_isbn_should_return_error_when_response_is_not_ok(self) -> None:
         # Arrange
         isbn = "9780123456789"
         responses.add(
             responses.GET,
-            f"{BookMetadataClient.OPEN_LIBRARY_API}isbn/{isbn}.json",
+            f"{BookMetadataClient.OPEN_LIBRARY_API}/api/books?bibkeys={isbn}&format=json&jscmd=details",
             json={},
             status=404,
         )
@@ -158,44 +225,10 @@ class TestBookMetadataClientOpenLibraryByIsbn:
         assert status == 404
         assert book == {}
 
-    @responses.activate
-    def test_open_library_by_isbn_should_still_return_book_metadata_when_author_response_is_not_ok(self, monkeypatch: MonkeyPatch) -> None:
-        # Arrange
-        isbn = "9780123456789"
-        responses.add(
-            responses.GET,
-            f"{BookMetadataClient.OPEN_LIBRARY_API}isbn/{isbn}.json",
-            json={
-                "title": "Test Title",
-                "authors": [
-                    {
-                        "key": "/authors/OL1607920A"
-                    }
-                ],
-                "description": {
-                    "value": "A fine book.",
-                }
-            },
-            status=200,
-        )
-        responses.add(
-            responses.GET,
-            f"{BookMetadataClient.OPEN_LIBRARY_API}/authors/OL1607920A.json",
-            json={},
-            status=404,
-        )
-        client = BookMetadataClient()
-
-        # Act
-        book, status = client.open_library_by_isbn(isbn)
-        # Assert
-        assert status == 200
-        assert book == {"Summary": "A fine book.", "Title": "Test Title"}
-
 
 class TestBookMetadataClientOpenWikiByIsbn:
     @responses.activate
-    def test_open_wiki_by_isbn_should_return_book_metadata(self, monkeypatch: MonkeyPatch) -> None:
+    def test_open_wiki_by_isbn_should_return_book_metadata(self) -> None:
         # Arrange
         isbn = "9780123456789"
         responses.add(
@@ -228,9 +261,10 @@ class TestBookMetadataClientOpenWikiByIsbn:
             "Summary": "",
         }
         assert len(responses.calls) == 1
-    
+        assert responses.calls[0].request.url == f"{BookMetadataClient.OPEN_WIKI_API}isbn/{isbn}.json"
+
     @responses.activate
-    def test_open_wiki_by_isbn_should_return_error_when_response_is_not_ok(self, monkeypatch: MonkeyPatch) -> None:
+    def test_open_wiki_by_isbn_should_return_error_when_response_is_not_ok(self) -> None:
         # Arrange
         isbn = "9780123456789"
         responses.add(
--- ubiblio/routers/books/book_metadata_client.py
+++ ubiblio/routers/books/book_metadata_client.py
@@ -8,7 +8,7 @@ from ...vars import GOOGLE_BOOKS_API_KEY
 
 class BookMetadataClient:
     GOOGLE_BOOKS_API = "https://www.googleapis.com/books/v1/volumes"
-    OPEN_LIBRARY_API = "https://openlibrary.org/"
+    OPEN_LIBRARY_API = "https://openlibrary.org"
     OPEN_WIKI_API = "https://en.wikipedia.org/api/rest_v1/data/citation/mediawiki/"
     USER_AGENT = "ubiblio_bot/1.0 (https://github.com/seanboyce/ubiblio;)"
 
@@ -23,36 +23,30 @@ class BookMetadataClient:
             book = {}
             book["Title"] = raw_book["title"]
             book["Author"] = raw_book["authors"][0]
-            try:
-                book["Summary"] = raw_book["description"]
-            except Exception:
-                book["Summary"] = ""
+            book["Summary"] = raw_book.get("description", "")
             return book, 200
         return {}, response.status_code
 
     def open_library_by_isbn(self, isbn: str) -> tuple[dict[str, str], int]:
-        url = self.OPEN_LIBRARY_API + "isbn/" + str(isbn) + ".json"
+        url = self.OPEN_LIBRARY_API + f"/api/books?bibkeys={isbn}&format=json&jscmd=details"
         headers = {
             "User-Agent": self.USER_AGENT,
             "Accept-Encoding": "gzip",
         }
         response = requests.get(url, headers=headers)
         if response.ok:
-            raw_book = json.loads(response.text)
-            book = {}
-            book["Title"] = raw_book["title"]
-            author_url = str(raw_book["authors"][0]["key"])
-            time.sleep(1)
-            response = requests.get(
-                self.OPEN_LIBRARY_API + author_url + ".json", headers=headers)
-            if response.ok:
-                book["Author"] = json.loads(response.text)["personal_name"]
+            data = response.json()
+            entry = data.get(isbn, {})
+            book_details = entry.get("details", {})
+            book = {
+                "Title": book_details.get("title", ""),
+                "Summary": book_details.get("description", {}).get("value", ""),
+            }
+            authors = book_details.get("authors", [])
+            if len(authors) > 0 and "name" in authors[0]:
+                book["Author"] = authors[0]["name"]
             else:
-                print(response.status_code)
-            try:
-                book["Summary"] = raw_book["description"]["value"]
-            except Exception:
-                book["Summary"] = ""
+                book["Author"] = ""
             return book, 200
         else:
             return {}, response.status_code
